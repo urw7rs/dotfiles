fda43f0d12825c1ef684fa5f623387e3fbd95aeb
urw7rs <urw.7rs@icloud.com>
2023-02-01 15:56:38 +0900

    remove ycmd

--- after_link.sh
+++ after_link.sh
@@ -2,7 +2,3 @@
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-pushd ./vim/plugged/youcompleteme
-python3 install.py --all
-popd
